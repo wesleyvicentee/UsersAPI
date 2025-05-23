4dfe71e89d914dadb1a44d740a7b0bff29c7362b
Wesley Vicente <wesley@maismei.com.br>
2025-05-23 17:58:10 -0300

    Revert "adjust paths"
    This reverts commit 549ddbd9e6bab663aba5b56d127217d496c7fbeb.

--- src/server.js
+++ src/server.js
@@ -21,6 +21,8 @@ app.post('/users', async (req, res) => {
 });
 
 app.get('/users', async (req, res) => {
+    console.log(req.query)
+
     let users = []
 
     if(req.query) {
@@ -40,6 +42,8 @@ app.get('/users', async (req, res) => {
 });
 
 app.put('/users/:id', async (req, res) => {
+    console.log(req)
+
     await prisma.user.update({
         where: {
             id: req.params.id
